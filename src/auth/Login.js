8523d223444c1035aae8f4d1d815763c1a50fa46
Stephen Lowe <stephenlowe@Stephens-MacBook-Pro.local>
2020-03-17 14:18:57 -0400

    mesage

--- src/auth/Login.js
+++ src/auth/Login.js
@@ -9,7 +9,7 @@ const Login = (props) => {
 
 const handleSubmit = (event) => {
     event.preventDefault();
-    fetch(`${APIURL}/user/signin`, {
+    fetch(`${APIURL}user/signin`, {
         method: 'POST', 
         body: JSON.stringify({user: {username: username, password: password}}),
         headers: new Headers ({
