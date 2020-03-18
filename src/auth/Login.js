c4940eef4e731934b4bbb9985389046aa94fcc54
Stephen Lowe <stephenlowe@Stephens-MacBook-Pro.local>
2020-03-18 11:05:30 -0400

    added api to url

--- src/auth/Login.js
+++ src/auth/Login.js
@@ -9,7 +9,7 @@ const Login = (props) => {
 
 const handleSubmit = (event) => {
     event.preventDefault();
-    fetch(`${APIURL}user/signin`, {
+    fetch(`${APIURL}api/user/signin`, {
         method: 'POST', 
         body: JSON.stringify({user: {username: username, password: password}}),
         headers: new Headers ({
--- src/helpers/environment.js
+++ src/helpers/environment.js
@@ -5,7 +5,7 @@ switch (window.location.hostname) {
     // this is the local host name of your react app
     case 'localhost' || '127.0.0.1':
         // this is the local host name of your API
-        //APIURL = 'http://localhost:3001/';
+        APIURL = 'http://localhost:3001/';
         break;
     // this is the deployed react application
     case 'swl-client.herokuapp.com':
